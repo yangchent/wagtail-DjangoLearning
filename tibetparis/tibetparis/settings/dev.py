7b9877127f4e9f538e1c0bf426bff3180340fe36
yangchen <gyachungriom@gmail.com>
2022-02-04 17:47:47 +0100

    initial commit

---
+++ tibetparis/tibetparis/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-g*^2pyot)cr!#*(8is_fqe29+22gq93idr=bjz(hj+0rivk%*w'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
